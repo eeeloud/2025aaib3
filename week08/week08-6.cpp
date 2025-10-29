47c429a5e8a8fbe9e95b130ccf3603b12871d46a
eeeloud <aa0976643713@gmail.com>
2025-10-29 12:00:12 +0800

    week08

---
+++ week08/week08-6.cpp
@@ -0,0 +1,29 @@
+/// 璶ぃ璶糶
+/// week08-6.cpp
+/// FB刮柑, 计稰龟喷, Τ程穝紇,  6714 ぃ盽计(计堵瑌)
+/// Input: 4计(ぃ狡)
+/// 程-程
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    cout << "叫块4计(计ぃ狡):";
+    int n;
+    cin >> n;
+    for(int i=0; i<7; i++)
+    {
+        vector<int>a;
+        for(int i=0; i<4; i++){
+            a.push_back(n%10);
+            n = n/10;
+        }
+        sort(a.begin(), a.end());
+        int M = a[3]*1000+a[2]*100+a[1]*10+a[0];
+        int m = a[0]*1000+a[1]*100+a[2]*10+a[3];
+        n = M - m;
+        printf("%d-%d=%d\n", M, m, n);
+    }
+}
